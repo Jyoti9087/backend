375d871d29087d824724e63b520f73aa850c545c
Jyotiprangya Behera <jyotiprangyabehera@gmail.com>
2025-12-05 15:47:23 +0800

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js"
+
+// import express from "express"
+// const app=express()
+
+const connectDB=async ()=>{
+    try{
+     const connectioInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+     console.log(`\n MongoDB connected !! DB HOST:${connectioInstance.connection.host}`); // MongoDB connected !! DB HOST:ac-sbwxzkl-shard-00-00.xowcbsq.mongodb.net
+     
+
+    }catch(error){
+      console.log("MONGODB connection failed",error); //MONGODB connection failed Error: querySrv ENOTFOUND _mongodb._tcp.2025(if you change anything in uri)
+      //you can use process anywhere
+      process.exit(1)
+    }
+
+}
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,53 @@
+
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+  path:'./env'
+})
+
+
+connectDB()
+
+
+// import express from "express"
+// const app=express()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//whenever you connect to db,then use async await function and try catch.
+//1st approach
+// (async()=>{
+//     try{
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     app.on("error",()=>{
+//         console.log("ERROR:",error);
+//         throw error
+        
+//     })
+  
+//     app.listen(process.env.PORT,()=>{
+//         console.log(`App is listening on port ${process.env.PORT} `);
+        
+//     })
+
+//     }catch(error){
+//         console.error("ERROR:",error)
+//         throw error
+//     }
+// })()
